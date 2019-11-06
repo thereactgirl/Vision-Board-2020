2655b9df8885f5cca5940805cef381bce41a859b
leslie <rleslie1015@gmail.com>
2019-11-06 00:00:05 -0600

    basics

---
+++ visionboard/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Nav from "./components/Nav";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav  />
+      <header className="board">
+        <section>Dress Well</section>
+        <section>Drink Water</section>
+        <section>Work Ot</section>
+        <section></section>
+        <section></section> 
+        <section></section>
+      
+          Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ visionboard/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Nav = () => {
+    return (
+        <div className="nav-container">
+            <a></a>
+            <a>Add New Vision</a>
+        </div>
+    )
+}
+
+export default Nav;
